ab971ee46abbe3ddb10bb222d82b275988663d66
jim <jim@badgrape.net>
2017-08-11 12:39:28 -0400

    Workaround to correctly display line items in cart component

--- sample/client/src/app/components/cart/cart.component.ts
+++ sample/client/src/app/components/cart/cart.component.ts
@@ -26,33 +26,46 @@ export class CartComponent {
       }
 
   removeProduct(product) {
+    // Elements of cart array to be removed
     let itemsToRemove = this.cart.filter(item => {
+      // Determine removed items by variant id ...
       if (product.selectedVariant && item.selectedVariant) {
         return item.selectedVariant.id === product.selectedVariant.id;
       }
-
+      // ... or product id
       else {
         return item.id === product.id;
       }
     });
 
+    // Debugging
     console.log('Items to remove:\n');
     console.log(itemsToRemove);
 
+    // Remove items from cart (reducer method)
     itemsToRemove.forEach(item => {
       this.cartStore.removeFromCart(item);
-
-      let lineToRemove = this.lineItems.findIndex(line => {
-        if (line.selectedVariant && item.selectedVariant) {
-          return line.selectedVariant.id === item.selectedVariant.id;
-        }
-
-        else {
-          return line.id === item.id;
-        }
-      })
-      this.lineItems.splice(lineToRemove);
     });
+
+    // Remove entries from list of items displayed
+    // (Separate from actual cart array)
+    let lineToRemove = this.lineItems.findIndex(line => {
+      // Removed based on variant id ...
+      if (product.selectedVariant && line.selectedVariant) {
+        return line.selectedVariant.id === product.selectedVariant.id;
+      }
+      // ... or product id
+      else {
+        return line.id === product.id;
+      }
+    });
+
+    let removed = this.lineItems.splice(lineToRemove, 1);
+    
+    // Debugging 
+    console.log('Removed:\n');
+    console.log(removed);
+
   }
 
   variants(cart) {
@@ -60,38 +73,49 @@ export class CartComponent {
       item.selectedVariant).length;
   }
 
+  // Group multiple cart entries together with quantites > 1
   getLineItems() {
     let cartCopy = JSON.parse(JSON.stringify(this.cart)); // deep copy
 
     cartCopy.forEach(item => {
+      // Already item(s) in cart
       if (this.lineItems.length > 0) {
 
-        let productHasVariants = item.selectedVariant;
+        // Test if product ids match
         let matchingProducts = this.lineItems.filter(line => {
           return line.id === item.id;
         });
         let productsMatch = matchingProducts.length > 0;
+
+        // Test if product has variants
+        let productHasVariants = item.selectedVariant;
+        // Test if variants match
         let matchingVariants = this.lineItems.filter(line => {
-          return line.id === item.id && line.selectedVariant && line.selectedVariant.id === item.selectedVariant.id;
+          return line.id === item.id && line.selectedVariant &&
+              line.selectedVariant.id === item.selectedVariant.id;
         });
         let variantsMatch = matchingVariants.length > 0;
 
+        // Group entries according to product or variant id
         if (!productHasVariants && productsMatch) {
           matchingProducts[0].quantity += item.quantity;
         }
         else if (productHasVariants && variantsMatch) {
           matchingVariants[0].quantity += item.quantity;
         }
+        // No match: new entry
         else {
           this.lineItems.push(item);
         }
 
       }
+      // First item in cart
       else {
         this.lineItems.push(item);
       }
     });
 
+    // Debugging
     console.log('Line items:\n');
     console.log(this.lineItems);
     console.log('Cart:\n');
